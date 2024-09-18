8ff38e9a99081480233664d32e959ea66bbb30aa
Dtrusted <109005133+dtrustedt@users.noreply.github.com>
2024-09-18 22:43:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 import Background from '../components/Background/Background'
+import { useRouter } from 'next/router'
+
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('https://flapmax.com')
+    }, 5000)
+
+    return () => clearTimeout(timer) // Clean up the timer on unmount
+  }, [router])
+
   return (
     <>
       <Head>
@@ -12,3 +24,4 @@ export default function Home() {
     </>
   )
 }
+
